a833102f9d437619d40c87e6f1091bdeea22a81d
Dmitrii Kaliuzhnyi <dmitrii_kaliuzhnyi@epam.com>
2022-05-03 13:26:56 +0300

    -Add encoder for api key, add project URL

--- src/main/java/setup/BaseTest.java
+++ src/main/java/setup/BaseTest.java
@@ -1,22 +1,30 @@
 package setup;
 
-import pages.PageObject;
-import support.PropertiesProvider;
+import static java.lang.String.format;
+
 import io.appium.java_client.AppiumDriver;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+import pages.PageObject;
+import support.PropertiesProvider;
 
 public class BaseTest implements IDriver {
 
     private static AppiumDriver appiumDriver; // singleton
     private static PageObject po;
+    private String PROJECT_NAME;
+    private String APPIUM_HUB;
+    private String API_KEY;
     public static PropertiesProvider properties;
 
     @Override
@@ -26,41 +34,56 @@ public class BaseTest implements IDriver {
         return po;
     }
 
-    @Parameters({"platformName","appType","deviceName","browserName","app"})
+    @Parameters({"platformName", "appType", "deviceName", "udid", "browserName","app","appPackage","appActivity","bundleId"})
     @BeforeSuite(alwaysRun = true)
-    public void setUp(String platformName, String appType, String deviceName, @Optional("") String browserName, @Optional("") String app) throws Exception {
+    public void setUp(String platformName,
+                      String appType,
+                      @Optional("") String deviceName,
+                      @Optional("") String udid,
+                      @Optional("") String browserName,
+                      @Optional("") String app,
+                      @Optional("") String appPackage,
+                      @Optional("") String appActivity,
+                      @Optional("") String bundleId)
+        throws Exception {
         System.out.println("Before: app type - "+appType);
-        setAppiumDriver(platformName, deviceName, browserName, app);
+        setAppiumDriver(platformName, deviceName, udid, browserName, app, appPackage, appActivity, bundleId);
         setPageObject(appType, appiumDriver);
-        properties = new PropertiesProvider();
     }
 
-    @AfterSuite(alwaysRun = true)
-    public void tearDown() {
-        System.out.println("After");
-        appiumDriver.closeApp();
-        properties = null;
-    }
-
-    private void setAppiumDriver(String platformName, String deviceName, String browserName, String app){
+    private void setAppiumDriver(String platformName, String deviceName, String udid, String browserName,
+                                 String app, String appPackage, String appActivity, String bundleId) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //mandatory Android capabilities
-        capabilities.setCapability("platformName",platformName);
-        capabilities.setCapability("deviceName",deviceName);
+        capabilities.setCapability("platformName", platformName);
+        capabilities.setCapability("deviceName", deviceName);
+        capabilities.setCapability("udid", udid);
 
         if(app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
 
         capabilities.setCapability("browserName", browserName);
         capabilities.setCapability("chromedriverDisableBuildCheck","true");
 
+        // Capabilities for test of Android native app on EPAM Mobile Cloud
+        capabilities.setCapability("appPackage", appPackage);
+        capabilities.setCapability("appActivity", appActivity);
+
+        // Capabilities for test of iOS native app on EPAM Mobile Cloud
+        capabilities.setCapability("bundleId", bundleId);
+
         try {
-            appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
-        } catch (MalformedURLException e) {
+            properties = new PropertiesProvider();
+            PROJECT_NAME = properties.getProperty("project.name");
+            APPIUM_HUB = properties.getProperty("appium.hub");
+            API_KEY = URLEncoder.encode(properties.getProperty("api.key"), StandardCharsets.UTF_8.name());
+            appiumDriver = new AppiumDriver(new URL
+                (format("https://%s:%s@%s/wd/hub", PROJECT_NAME, API_KEY, APPIUM_HUB)), capabilities);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
         // Timeouts tuning
-        appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        appiumDriver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
 
     }
 
@@ -68,5 +91,11 @@ public class BaseTest implements IDriver {
         po = new PageObject(appType, appiumDriver);
     }
 
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        System.out.println("After");
+        appiumDriver.closeApp();
+    }
 
 }
